595df8fa44338a77a9e9c3bbbcd3be6dc47eb380
Muhammad Fahied <fahied@gmail.com>
2025-05-07 13:16:30 +0400

    added simple calculator

---
+++ MeasureSwift/Logger.swift
@@ -0,0 +1,15 @@
+//
+//  Logger.swift
+//  MeasureSwift
+//
+//  Created by Muhammad Fahied on 07/05/2025.
+//
+
+public class Logger {
+    public init() {
+        
+    }
+    func log(_ message: String) {
+        print("LOG: \(message)")
+    }
+}
