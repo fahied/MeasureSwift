64b8ed7ad978f140058a9eb837d3c58ed84d8c16
Muhammad Fahied <fahied@gmail.com>
2025-05-07 11:13:49 +0400

    initial commit

---
+++ MeasureSwift/MeasureSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureSwiftApp.swift
+//  MeasureSwift
+//
+//  Created by Muhammad Fahied on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MeasureSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeasureSwiftTests/MeasureSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureSwiftTests.swift
+//  MeasureSwiftTests
+//
+//  Created by Muhammad Fahied on 07/05/2025.
+//
+
+import Testing
+@testable import MeasureSwift
+
+struct MeasureSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
